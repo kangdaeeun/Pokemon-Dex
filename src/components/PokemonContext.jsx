37a397495696270703fb1f881cc3a1edce211f2b
kangdaeeun <daeeun123@daum.net>
2024-11-14 12:52:37 +0900

    찐찐

--- src/components/PokemonContext.jsx
+++ src/components/PokemonContext.jsx
@@ -6,17 +6,15 @@ const PokemonProvider = ({ children }) => {
   const [selectedPokemon, setSelectedPokemon] = useState([]);
   const AddPokemon = (e, pokemon) => {
     e.preventDefault();
-    e.stopPropagation();    // 상위 태그에 이벤트 전파를 막아준다
+    e.stopPropagation(); // 상위 태그에 이벤트 전파를 막아준다
     const mons = MOCK_DATA;
     const newPokemon = mons.find((mon) => {
       return mon.id === pokemon.id;
     }); // ID에 맞는 포켓몬 찾기
-    if (selectedPokemon.length >= 6) {
-      alert("내 꼬는 6마리가 최대야");
-    } else if (
-      selectedPokemon.some((prevPokemon) => prevPokemon.id === pokemon.id)
-    ) {
+    if (selectedPokemon.some((prevPokemon) => prevPokemon.id === pokemon.id)) {
       alert("내 꼬에 있는 포켓몬이야");
+    } else if (selectedPokemon.length >= 6) {
+      alert("내 꼬는 6마리가 최대야");
     } else {
       setSelectedPokemon((prevSelected) => [...prevSelected, newPokemon]);
     }
